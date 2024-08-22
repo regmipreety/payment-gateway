38295a51cb11a5689d6be0f1367b74f03a6ea667
regmipreety <priety.regmi46@gmail.com>
2024-08-21 22:30:06 -0500

    Handle approval for Stripe service

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -33,7 +33,7 @@ class PaymentController extends Controller
             'currency' => ['required', 'exists:currencies,iso'],
             'payment_platform' => ['required', 'exists:payment_platforms,name'],
         ];
-        dd($request->all());
+       
         $request->validate($rules);
         $paymentPlatform = $this->paymentPlatformResolver
             ->resolveService($request->payment_platform);
--- app/Services/StripeService.php
+++ app/Services/StripeService.php
@@ -31,30 +31,66 @@ class StripeService
 
     public function resolveAccessToken()
     {
-       return "Bearer {$this->secret}";
+        return "Bearer {$this->secret}";
     }
 
     public function handlePayment(Request $request)
     {
-       
+        $request->validate([
+            'payment_method' => 'required',
+        ]);
+        $intent = $this->createIntent($request->value, $request->currency, $request->payment_method);
+        session()->put('paymentIntentId', $intent->id);
+        return redirect()->route('approval');
     }
 
     public function handleApproval()
     {
-       
+        if(session()->has('paymentIntentId')){
+            $paymentIntentId = session()->get('paymentIntentId');
+            $confirmation = $this->confirmPayment($paymentIntentId);
+            if($confirmation->status === 'succeeded'){
+                $name = auth()->user()->name;
+                $currency = strtoupper($confirmation->currency);
+                $amount = $confirmation->amount/$this->resolveFactor($currency);
+                return redirect()
+                ->route('home')
+                ->withSuccess(['payment' => "Thanks, {$name}. We received your {$amount}{$currency} payment."]);
+            }
+        }
+        return redirect()
+        ->route('home')
+        ->withErrors('We are unable to confirm your payment.');
     }
 
-    public function createIntent($value, $currency, $paymentMethod){
+    public function createIntent($value, $currency, $paymentMethod)
+    {
         return $this->makeRequest(
             'POST',
             '/v1/payment_intents',
             [],
             [
-                'amount'=> round($value * $this->resolveFactor($currency)),
-                'currency'=>strtolower($currency),
-                'payment_method'=> $paymentMethod,
-                'confirmation_method'=>'manual',
+                'amount' => round($value * $this->resolveFactor($currency)),
+                'currency' => strtolower($currency),
+                'payment_method' => $paymentMethod,
+                'confirmation_method' => 'manual',
             ]
+        );
+    }
+
+    public function confirmPayment($paymentIntentId)
+    {
+        return $this->makeRequest(
+            'POST',
+            "/v1/payment_intents/{$paymentIntentId}/confirm",
+            [],
+
+            [
+
+                'return_url' => route('home'),
+            ]
+
+
         );
     }
 
